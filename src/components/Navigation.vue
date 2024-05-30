89466f93ac2f7e95d021436c46357c49717cc22c
Bereket Melese <133118332+BereketMelese1@users.noreply.github.com>
2024-05-30 22:56:29 +0300

    Update Navigation.vue

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,7 +1,11 @@
 <template>
   <nav class="bg-gray-800 fixed w-full top-0 z-10 p-4">
     <div class="container mx-auto flex justify-between items-center">
-      <div class="text-white text-xl font-bold ml-10">Rick And Morty</div>
+      <div class="text-white text-xl font-bold ml-10">
+        <router-link to="/" class="text-white">
+          Rick And Morty
+        </router-link>
+      </div>
       <div>
         <ul class="flex space-x-4 mr-12">
           <li>
