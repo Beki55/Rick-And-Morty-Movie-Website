7c907515010dcaff1f75d2dd61a2fa4f0ffc747c
Bereketmelese1 <Bereket21bnm@gmail.com>
2024-06-09 23:13:44 +0300

    commited

--- src/components/CharacterSearch.vue
+++ src/components/CharacterSearch.vue
@@ -1,15 +1,15 @@
 <template>
-  <section>
-    <div class="my-48 flex justify-center">
+  <section class="p-4">
+    <div class="flex justify-center items-center min-h-screen">
       <div>
-        <p class="text-3xl mb-4 ml-4">
+        <p class="text-2xl sm:text-3xl mb-4 ml-4 text-center sm:text-left">
           Enter the id of character you want to search
         </p>
 
         <input
           v-model="searchQuery"
           @keyup.enter="handleSearchInput"
-          class="m-4 shadow-lg border rounded-3xl w-[650px] py-2 px-3 text-gray-700 pl-10"
+          class="m-4 shadow-lg border rounded-3xl lg:w-[650px] w-full py-2 px-3 text-gray-700"
           type="text"
           placeholder="type 1,2,3..."
         />
